547cc608982e08b01a60287dd88bf5655a2a4162
Soslan <sparksoslan@gmail.com>
2020-04-02 14:44:40 +0100

    Sidebar styling and Jokes panel

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './JokeList.css';
 
 class JokeList extends Component {
   static defaultProps = {
@@ -25,7 +26,16 @@ class JokeList extends Component {
     const jokes = this.state.jokes.map(joke => <div>{joke}</div>);
     return (
       <div className="JokeList">
-        <h1>JokesList</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="laughing emoji"
+          />
+          <button className="JokeList-getmore">More Jokes</button>
+        </div>
         <div className="JokeList-jokes">{jokes}</div>
       </div>
     );
